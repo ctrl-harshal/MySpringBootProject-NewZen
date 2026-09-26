9d050f7c2b6568bf0df15cf1cbb0ce2e2cbe55b1
ctrl-harshal <harshaltamgadge@gmail.com>
2026-09-26 10:58:17 +0530

    Change RequestMapping to GetMapping for home method

--- src/main/java/com/web/controller/MainController.java
+++ src/main/java/com/web/controller/MainController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+	@RequestMapping("/api")
 public class MainController {
 
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String home() {
 		System.out.println("This is a home page for harshal");
 		return "home";
